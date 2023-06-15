87b8a709048d90e2676bc67247c5532a961ac121
AmarachiGeorge <chinazaamarachi64@gmail.com>
2023-06-15 14:26:56 -0700

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/*
+ * The main function is the entry point of the program.
+ * It prints a message to the console and returns 0 to indicate successful execution..
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+	puts("Programming is like building a "
+         "multilingual puzzle");
+	retturn 0;
 }
