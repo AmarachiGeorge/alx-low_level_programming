277db08bdee7907db28c18ba85a882240f7172d1
AmarachiGeorge <chinazaamarachi64@gmail.com>
2023-06-21 15:57:35 -0700

    update

--- 0x02-functions_nested_loops/24_hours.c
+++ 0x02-functions_nested_loops/24_hours.c
@@ -6,23 +6,20 @@
 void jack_bauer(void)
 {
 	int i, j;
-
 	i = 0;
-
 	while (i < 24)
 	{
-		i = 0;
-
+		j = 0;
 		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(',');
-			_putchar((j % 10) + '0');
+			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
-			j++;
+			j++
 		}
-		j++;
+		j++
 	}
 }
