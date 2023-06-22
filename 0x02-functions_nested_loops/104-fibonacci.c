8cfffb6788c45cbafe295a22301292a1bad536ed
AmarachiGeorge <chinazaamarachi64@gmail.com>
2023-06-21 17:04:00 -0700

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,36 +8,40 @@
  */
 int main(void)
 {
-        unsigned long int i;
-        unsigned long int bef = 1;
-        unsigned long int aft = 2;
-        unsigned long int l = 1000000000;
-        unsigned long int bef1;
-        unsigned long int bef2;
-        unsigned long int aft1;
-        unsigned long int aft2;
-        printf("%lu", bef);
-        for (i = 1; i < 91; i++)
-        {
-                printf(", %lu", aft);
-                aft += bef;
-                bef = aft - bef;
-        }
-        bef1 = (bef / l);
-        bef2 = (bef % l);
-        aft1 = (aft / l);
-        aft2 = (aft % l);
-        for (i = 92; i < 99; ++i)
-        {
-                printf(", %lu", aft1 + (aft2 / l));
-                printf("%lu", aft2 % l);
-                aft1 = aft1 + bef1;
-                bef1 = aft1 - bef1;
-                aft2 = aft2 + bef2;
-                bef2 = aft2 - bef2;
-        }
-        printf("\n");
-        return (0);
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
+
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
+
+	return (0);
 }
 _putchar.c
 #include "main.h"
@@ -51,11 +55,12 @@ _putchar.c
  */
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 main.h
 #ifndef main_h
 #define main_h
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -70,3 +75,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 #endif
+
